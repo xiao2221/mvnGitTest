fbfa92c32e65a3281d87f0487ab6fe29f86af760
xiaoliang <xiaoliang@xiaoliang.com>
2018-08-27 19:09:14 +0800

    0.1v

---
+++ src/main/java/com/maven/Person.java
@@ -0,0 +1,44 @@
+package com.maven;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String [] args){
+        System.out.println("main in");
+        Person per=new Person("tom",34);
+        System.out.println(per);
+    }
+}
