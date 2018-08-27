595aeebf644e1ef80f38fd36845bc7d65e29de8a
xiaoliang <xiaoliang@xiaoliang.com>
2018-08-27 19:27:44 +0800

    0.3v

--- src/main/java/com/maven/Test.java
+++ src/main/java/com/maven/Test.java
@@ -3,7 +3,9 @@ package com.maven;
 public class Test {
     public static void main(String[] args){
         System.out.println("test main");
-        Person per=new Person();
+        Person per=new Person("jack",33);
         System.out.println(per);
+        Dog dog=new Dog("大黄",3);
+        System.out.println(dog);
     }
 }
